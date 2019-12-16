97f0288345a8c2c0f018526cad24e3591cfda17a
SushGit <annettesushanthi92@gmail.com>
2019-12-15 22:15:17 -0500

    second

---
+++ second-java-project/src/main/java/com/test/maven/App.java
@@ -0,0 +1,14 @@
+package com.test.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Vickyyyyyy!!!" );
+        System.out.println( "HOW ARE YOU???" );
+    }
+}
